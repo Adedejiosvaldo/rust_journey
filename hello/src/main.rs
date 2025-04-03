fb28582c2dfebc1773aa0519659ca36084d6f4cf
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-04-03 20:44:30 +0100

    Feature: Added arrays and tuples

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -9,13 +9,41 @@ fn main() {
     let decimal_number: f32 = 1.0;
 
     // char
-    let char_code = "c";
+    let _char_code = "c";
 
     // unicode
 
     // boolean
+    let is_today_blue: bool = true;
+
+    println!(
+        "Printing variables-  signed: {}, unsigned: {}, float:{}",
+        signed, age, decimal_number
+    );
 
     println!("his age is {}", age);
 
-    println!("Hello, world!");
+    println!("Hello, world!, is today blue? : {}", is_today_blue);
+
+    // arrays
+    // create an array of type u8 with a size of 5
+    let arr: [u8; 5] = [1, 2, 3, 5, 6];
+
+    // printing the structure of an array - like looping through an array
+    println!("{:?}", arr);
+
+    // tuple
+    // hold elemnets of different values together
+    let example_tuple: (i8, bool, f32) = (10, false, 3.0);
+    // or we could create a tuple using
+    let _tup_2 = (1, 4, true);
+
+    // printing out a tuple
+    println!("first tup {}", example_tuple.1);
+    // printing structure
+    println!("{:?}", example_tuple);
+
+    // destructuring a tuple
+    let (a, b, c) = example_tuple;
+    println!("a: {}, b: {}, c: {}", a, b, c);
 }
