34e73b07a6ccad24ee96c4af27411aefaa84791c
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-04-03 20:30:02 +0100

    Feature: Added variable types

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,6 +2,19 @@ fn main() {
     //  unsigned
     let age: u8 = 10;
 
+    // Signed i2,i4,i8,i16,i32,i64
+    let signed: i64 = -59;
+
+    // float
+    let decimal_number: f32 = 1.0;
+
+    // char
+    let char_code = "c";
+
+    // unicode
+
+    // boolean
+
     println!("his age is {}", age);
 
     println!("Hello, world!");
